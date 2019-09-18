fb0512737f2da2cdefbb2eb4f8f89a2209ab4d7b
2778305423 <2778305423@qq.com>
2019-09-18 21:08:28 +0800

    登陆界面头部的公共组件

---
+++ zhihuishu/src/components/LoginTop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo">
+    <img
+      src="https://image.zhihuishu.com/zhs/ablecommons/demo/201812/e67093aa50fa477d842b98fe8b04d645.png"
+    />
+  </div>
+</template>
+<style>
+
+.logo {
+  width: 400px;
+  height: 70px;
+  position: relative;
+}
+.logo img {
+  width: 152px;
+  height: 40px;
+  position: absolute;
+  left: 124px;
+  bottom: 0;
+}
+
+</style>
